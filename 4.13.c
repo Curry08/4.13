3fbb07e502cc07bd572c9b9268a005386de6884a
anwar <anwarcourri12@gmail.com>
2024-10-19 19:29:15 +0200

    .

--- 4.13.c
+++ 4.13.c
@@ -12,35 +12,37 @@ int main(){
     printf("5)Esci\n");
     printf("Scelta:");
     scanf("%d",&scelta);
-
+    while(1)
     switch(scelta){
             case(1):
         printf("Inserisci il primo numero: ");
         scanf("%f",&num1);
         printf("Inserisci il secondo numero: ");
         scanf("%f",&num2);
-            printf("Il risultato è: %f",num1+num2);break;
+        printf("Il risultato è: %f",num1+num2);
+        return 0;
             case(2):
         printf("Inserisci il primo numero: ");
         scanf("%f",&num1);
         printf("Inserisci il secondo numero: ");
-        scanf("%f",&num2);printf("Il risultato è: %f ",num1-num2);break;
+        scanf("%f",&num2);printf("Il risultato è: %f ",num1-num2);
+        return 0;
             case(3):
         printf("Inserisci il primo numero: ");
         scanf("%f",&num1);
         printf("Inserisci il secondo numero: ");
-        scanf("%f",&num2);printf("Il risultato è: %f ",num1*num2);break;
+        scanf("%f",&num2);printf("Il risultato è: %f ",num1*num2);
+        return 0;
             case(4):
         printf("Inserisci il primo numero: ");
         scanf("%f",&num1);
         printf("Inserisci il secondo numero: ");
-        scanf("%f",&num2);printf("Il risultato è: %f ",num1/num2);break;
-            case(5):
-                break;
+        scanf("%f",&num2);printf("Il risultato è: %f ",num1/num2);
+        return 0;
+            case(5):return 0;
         default:
-        printf("Opizione inesistente");
-        break;
-
+        printf("Opizione inesistente\n");
+        printf("Scelta:");
+        scanf("%d",&scelta);
     }
-    return 0;
 }
